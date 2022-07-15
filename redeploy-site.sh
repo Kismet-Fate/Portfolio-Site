7c0c2953691be14f23acdb70bc2b0c796f556ee2
Kismet-Fate <yoloswagftw545@gmail.com>
2022-07-15 16:38:53 -0400

    update

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,4 +1,4 @@
 cd ~/project-chad-wojak
 git fetch && git reset origin/main --hard
-docker compose -f docker-compose.prod.yml down
-docker compose -f docker-compose.prod.yml up -d --build
+docker compose -f docker-compose.yml down
+docker compose -f docker-compose.yml up -d --build
